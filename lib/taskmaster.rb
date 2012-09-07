a7168d82d548b762e57fb12a213d88881fefd818
Jacob Evans <jacob@dekz.net>
2012-09-07 15:05:23 +1000

    Taskmaster

---
+++ lib/taskmaster.rb
@@ -0,0 +1,70 @@
+require 'drb'
+require 'rinda/ring'
+require 'utils'
+require 'job'
+
+# Avengers Ahoy!
+class Taskmaster
+  def initialize(ts=nil)
+    @ts = ts
+    if ts.nil?
+      DRb.start_service
+      @ts = Rinda::RingFinger.primary
+    end
+  end
+
+  ##
+  # Runs the job on a remote worker. Args passed in here
+  # will either be Marshalled or a Reference (DRbObject)
+  # will be sent over.
+  def run_job *args, &block
+    require 'sourcify'
+    block_string = block.to_source
+    jt = Job::START_TEMPLATE.dup
+    jt['id'] = Utils::random_str
+    jt['run'] = {
+      'proc' => block_string,
+      'args' => args
+    }
+    @ts.write jt
+    jt['id']
+  end
+
+  def clear_jobs
+    stopped = []
+    st = Job::STOP_TEMPLATE
+    stop = @ts.take(st, 0) rescue nil
+    while !stop.nil?
+      stopped << stop
+      stop = @ts.take(st, 0) rescue nil
+    end
+
+    jt = Job::START_TEMPLATE
+    stop = @ts.take(jt, 0) rescue nil
+    while !stop.nil?
+      stopped << stop
+      stop = @ts.take(jt, 0) rescue nil
+    end
+    stopped
+  end
+
+  def completed_jobs timeout=0
+    stopped = []
+    while stop = jobs_complete?(1, timeout)
+      stopped << stop
+    end
+    stopped
+  end
+
+  def jobs_complete? num=1, timeout=10
+    completed = nil
+    num.times do 
+      jt = Job::COMPLETE_TEMPLATE
+      job =  @ts.take(jt, timeout) rescue nil
+      return completed unless job
+      completed ||= []
+      completed << job
+    end
+    completed
+  end
+end
--- test.rb
+++ test.rb
@@ -5,6 +5,7 @@ require 'rinda/ring'
 $:.unshift File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
 require 'utils'
 require 'job'
+require 'taskmaster'
 
 DRb.start_service
 #$ts = DRbObject.new_with_uri('druby://:12345')
@@ -29,55 +30,7 @@ stop_job['id'] = job['id']
 stop_job2 = Job::STOP_TEMPLATE
 stop_job2['id'] = job2['id']
 
-def clear_jobs
-  stopped = []
-  st = Job::STOP_TEMPLATE
-  stop = $ts.take(st, 0) rescue nil
-  while !stop.nil?
-    stopped << stop
-    stop = $ts.take(st, 0) rescue nil
-  end
-
-  jt = Job::START_TEMPLATE
-  stop = $ts.take(jt, 0) rescue nil
-  while !stop.nil?
-    stopped << stop
-    stop = $ts.take(jt, 0) rescue nil
-  end
-  stopped
-end
-
-def completed_jobs timeout=0
-  stopped = []
-  while stop = jobs_complete?(1, timeout)
-    stopped << stop
-  end
-  stopped
-end
-
-def jobs_complete? num=1, timeout=10
-  completed = nil
-  num.times do 
-    jt = Job::COMPLETE_TEMPLATE
-    job =  $ts.take(jt, timeout) rescue nil
-    return completed unless job
-    completed ||= []
-    completed << job
-  end
-  completed
-end
-
-def run_job *args, &block
-  require 'sourcify'
-  block_string = block.to_source
-  jt = Job::START_TEMPLATE.dup
-  jt['run'] = {
-    'proc' => block_string,
-    'args' => args
-  }
-  p jt
-  $ts.write jt
-end
+$tm = Taskmaster.new $ts
 
 require 'irb'
 IRB.start
